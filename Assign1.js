e66b34c6513242f6fafc67feac187aec9226e364
Brad Bridge <bradbridge@Brads-MacBook-Pro.local>
2016-06-29 09:57:47 -0700

    first commit

---
+++ Assign1.js
@@ -0,0 +1,202 @@
+            //Assignment1
+            /*
+            //STEP 1
+            //varsome_month
+            var someMonth;
+            //function theMonth
+            function theCurrentMonth(){}
+            //current-month
+            const CURRENT_MONTH = 12;
+            //varsummer_month
+            var arrSummerMonths = ["Jan","Feb","Mar","Apr","May","jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+            //MyLibrary-afunction
+            function myLibaryAFunction(){}
+            */
+
+            /*
+            //STEP 2
+            //numeric literal exprssion
+            34.56
+            //string literal expression
+            "Hello World"
+            //boolean literal expression
+            true
+            //null literal exprssion
+            null
+            */
+            
+            /*
+            //STEP 3
+            //complex variable expressions
+            var totalAmt = 23 + 16.94;
+            var myName = "Tom" + "Jones";
+            */
+            
+            /*
+            //STEP 4
+            var strFirstName, strLastName, strAddress, strCity, strState, strZipCode, intYourAge, strReferalSource, boolMayWeContactYou
+            */
+            
+            /*
+            //STEP 5
+            //hungarian notation
+            strFirstName = "Joe"; strLastName = "Smithh"; strAddress = "85 lawn St"; strCity = "SD"; strZipCode = "92114";
+            innYourAge = 35;  strReferalSource = "Web"; boolMayWeContactYou = false;
+            //assignment methods
+            //assign on declaration
+            var num = 28;
+            //assign after declaration
+            num = 28;
+            //assign multiple variables
+            num1 = num2 = 97;
+            */
+            
+            /*
+            //STEP 6
+            var var1 = 40 + "30";
+            window.console.log(var1);
+            
+            var var2 = false + "true";
+            window.console.log(var2);
+            
+            var var3 = 26 + false;
+            window.console.log(var2);
+            */
+            
+            /*
+            //STEP 7
+            //hoisting - assignment after declaration
+            number = 34;
+            var number;
+            */
+            
+            /*
+            //STEP 8
+            var someString = "Who once said, \"Only two things are infinite, the universe and human stupidity, I\'m not sure about the former.";
+            */
+            
+            /*
+            //STEP 9
+            var nullValue = "";
+            window.console.log(nullValue);
+            
+            var undefValue;
+            window.console.log(undefValue);
+            */
+            
+            /*
+            //STEP 10
+            var strVar = "string";
+            var numVar = 34.56;
+            var boolVar = true;
+            var objVar = new Object;
+            var undefVar;
+            
+            window.console.log(typeof strVar);
+            window.console.log(typeof numVar);
+            window.console.log(typeof boolVar);
+            window.console.log(typeof objVar);
+            window.console.log(typeof undefVar);
+            */
+            
+            /*
+            //STEP 11
+            alert("Hello " + "Zak Ruvalcaba" + " welcome to the javascript class");
+            */
+        
+            /*
+            //STEP 12
+            var strMyName = "Brad Bridge";
+            alert("Hello " + strMyName + " welcome to the javascript class");
+            */
+            
+            /*
+            //STEP 13
+            var strCourse = "javascript";
+            alert("Hello " + strMyName + " welcome to the " + strCourse + " class");
+            */
+            
+            /*
+            //STEP 14
+            alert("Hello " + strMyName + "<br>Welcome to the " + strCourse + " class");
+            */
+            
+            /*
+            //STEP 15
+            var strName = window.prompt("Enter you name:");
+            alert("Hello " + strName + "<br>Welcome to the " + strCourse + " class");
+            */
+
+            /*
+            //STEP 16
+            var strCourse = window.prompt("Enter you course:");
+            alert("Hello " + strName + "<br>Welcome to the " + strCourse + " class"); 
+            */
+            
+            /*
+            //STEP 17
+            var x = 10;
+            var y = 20;
+            window.console.log(x + y);
+            */
+            
+            /*
+            //STEP 18
+            var x = 20;
+            x += 20;
+            window.console.log(x);
+            */
+            
+            /*
+            //STEP 19
+            var x = 20;
+            x *= 5;
+            window.console.log(x);
+            */
+            
+            /*
+            //STEP 20
+            var x = 20 % 3;
+            x /= 1;
+            window.console.log(x);
+            */
+            
+            /*
+            //STEP 21
+            var x = (10>5 && 20>6);
+            windows.console.log(x);
+            */
+            
+            /*
+            //STEP 22
+            var x = (5>10 || !(8>4));
+            window.console.log(x);
+            */
+            
+            /*
+            //STEP 23
+            var oWidget = new Object;
+            windows.console.log(typeof oWidget);
+            */
+            
+            /*
+            //STEP 24
+            var boolResult = (oWideget instanceof object);
+            if (boolResult){
+                window.console.log("oWidget is an object");
+            } else {
+                window.console.log("oWidget is not an object");
+            }
+            */
+
+            /*
+            //STEP 25
+            var boolResult = !(oWideget instanceof object);
+            if (boolResult){
+                window.console.log("oWidget is not an object");
+            } else {
+                window.console.log("oWidget is an object");
+            }
+            */
+//END
+       
